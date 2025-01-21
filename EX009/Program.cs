7872848127f4cc3c5b0a9b17e5f9fbfa4b4e24f9
thiago <thiago.moliveira31@senacsp.edu.br>
2025-01-21 20:13:28 -0300

    Identificação de triângulos

--- EX009/Program.cs
+++ EX009/Program.cs
@@ -16,14 +16,28 @@
             Console.Write("Digite o valor do lado C:");
             LadoC = Convert.ToInt32(Console.ReadLine());
 
-            if ((LadoA < LadoB + LadoC) && (LadoB < LadoA + LadoC) && (LadoC < LadoA + LadoB)) 
+            if ((LadoA < LadoB + LadoC) && (LadoB < LadoA + LadoC) && (LadoC < LadoA + LadoB))
             {
                 Console.WriteLine("Os valores informados formam um triângulo");
+                if ((LadoA == LadoB) && (LadoB == LadoC)) 
+                {
+                    Console.WriteLine("E ele é um triângulo Equilátero.");
+                }
+                else if ((LadoA == LadoB) || (LadoA == LadoC) || (LadoB == LadoC))
+                {
+                    Console.WriteLine("Ele é um triângulo Isósceles.");
+                }
+                else
+                {
+                    Console.WriteLine("Ele é um triângulo Escaleno.");
+                }
+     
             }
             else
             {
                 Console.WriteLine("Os valores informados não formam um triângulo.");
             }
+
 
 
         }
