2a82ea6445733a9c6da8457db814266dafdba73e
thiago <thiago.moliveira31@senacsp.edu.br>
2025-01-21 22:10:34 -0300

    Salario

---
+++ EX10/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.Design;
+
+namespace EX10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aumento de salario");
+            Double salario, salariofinal;
+            Console.WriteLine("Quanto você recebe?");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            if (salario > 2500)
+            {
+                salariofinal = (salario * 0.05) + salario;
+                Console.WriteLine("Seu aumento é de: {0}", salariofinal);
+            }
+            else if (salario > 1250)
+            {
+                salariofinal = (salario * 0.10) + salario;
+                Console.WriteLine("O seu aumento é de:{0}", salariofinal);
+            }
+            else
+            {
+                salariofinal = (salario * 0.15) + salario;
+                Console.WriteLine("O seu aumento é de:{0}", salariofinal);
+            }
+                
+            
+            
+
+            
+            
+            
+            
+
+            
+                
+            
+                
+            
+                  
+                    
+
+                
+
+              
+            
+            
+            
+               
+                
+              
+             
+            
+
+            
+
+
+        }
+    }
+}
