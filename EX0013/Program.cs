55830476bd964aa9058da510a6c7f0a87794f2e1
thiago <thiago.moliveira31@senacsp.edu.br>
2025-01-28 19:46:17 -0300

    Senha

---
+++ EX0013/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EX0013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+            /*
+            while (num<5)
+            {
+                num++;
+                Console.WriteLine("Curso C#");
+            }
+            */
+            do
+            {
+                num++;
+                Console.WriteLine("Curso c#");
+
+            }while(num<5);
+        }   
+    }
+}
---
+++ Senha/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123"; // senha correta
+            string senhadigitada; // senha digitada pelo usuário
+            int tentativas = 0; // contador de tantativas
+            do
+            {
+                Console.Clear();
+                Console.Write("Digite a senha");
+                senhadigitada = Console.ReadLine();
+                tentativas++;
+
+                if (senhadigitada != senha && tentativas < 3)
+                {
+                    Console.WriteLine("Senha incorreta!");
+                    Console.WriteLine("voce tem ainda " + (3 - tentativas) + "tentativa (s)");
+                    Console.ReadKey();
+                }
+                else if(senhadigitada != senha && tentativas ==3)
+                {
+                    Console.WriteLine("senha incorreta! Seu acesso está bloqueado!");
+                    return;
+                }
+            } while (senhadigitada != senha);
+
+            Console.Clear() ;
+            Console.WriteLine("Senha correta! - Tentativas " + tentativas);
+        }
+    }
+}
