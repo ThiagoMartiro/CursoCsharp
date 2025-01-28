8a927a015ed9c37e7e5e0f9c28af35ecad4e967f
thiago <thiago.moliveira31@senacsp.edu.br>
2025-01-28 20:16:38 -0300

    vetores

---
+++ vetor/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+
+            num[0] = 10;
+            num[1] = 20;
+            num[2] = 30;
+            num[3] = 40;
+            num[4] = 50;
+            // num [5] = 60; erro, passou dos limies do vetor. Vetor só vai até 4 posições.
+            Console.WriteLine(num[2]);
+
+            int[] n = new int[5] { 60, 70, 80, 90, 100 };
+
+            Console.WriteLine(n[0]);
+
+            int[] numero = { 65, 25, 84, 76, 12, 8 };
+            Console.WriteLine(numero[5]);
+            
+           
+
+        
+        }
+
+    }
+}
